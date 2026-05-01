2ac45cc0f7cdf4e9e454413061919dd850f70f79
AlvesAnalu <marsilioanalu@gmail.com>
2026-05-01 19:02:48 -0300

    socorro

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -105,14 +105,11 @@ def calcula_pontos_quina(face_dados6):
         return 0
     
 def calcula_pontos_regra_avancada(dados):
-    soma = 0
-    for x in dados:
-        soma += x
     return {
         'cinco_iguais': calcula_pontos_quina(dados),
         'full_house': calcula_pontos_full_house(dados),
         'quadra': calcula_pontos_quadra(dados),
-        'sem_combinacao': soma,
+        'sem_combinacao': calcula_pontos_soma(dados),
         'sequencia_alta': calcula_pontos_sequencia_alta(dados),
         'sequencia_baixa': calcula_pontos_sequencia_baixa(dados)
     }
