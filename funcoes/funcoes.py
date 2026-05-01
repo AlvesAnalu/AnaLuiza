4c8a6600cf404fe42dcddea5c1dda8b3bf9ab0a7
AlvesAnalu <marsilioanalu@gmail.com>
2026-05-01 19:00:57 -0300

    eujuroqueeutotentando

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -109,7 +109,6 @@ def calcula_pontos_regra_avancada(dados):
     for x in dados:
         soma += x
     return {
-        'soma': calcula_pontos_soma(dados),
         'cinco_iguais': calcula_pontos_quina(dados),
         'full_house': calcula_pontos_full_house(dados),
         'quadra': calcula_pontos_quadra(dados),
