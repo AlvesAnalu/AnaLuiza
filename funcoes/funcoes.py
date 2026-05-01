1858272f4829a1cefc04aacc50c4e9228dfe7ef4
AlvesAnalu <marsilioanalu@gmail.com>
2026-05-01 16:06:39 -0300

    versao3

--- funcoes/funcoes.py
+++ funcoes/funcoes.py
@@ -9,6 +9,6 @@ def rolar_dados(quantos_dados):
 def guardar_dado(dados_rolados, dados_guardados, indice):
     valor= dados_rolados[indice]
     novos_rolados= dados_rolados+[valor]
-    novos_guardados= n_guardados+[valor]
+    novos_guardados= novos_guardados+[valor]
     dados_escolhidos= [novos_rolados, novos_guardados]
     return dados_escolhidos
