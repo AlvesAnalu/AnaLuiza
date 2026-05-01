fd8685deb839ff7b1dc91d2e51dcc9231ca5bc29
AlvesAnalu <marsilioanalu@gmail.com>
2026-05-01 16:06:07 -0300

    versao2

---
+++ funcoes/funcoes.py
@@ -0,0 +1,14 @@
+import random
+
+def rolar_dados(quantos_dados):
+    resultados_dados= []
+    for i in range(quantos_dados):
+        resultados_dados.append(random.randint(1,6))
+    return resultados_dados
+
+def guardar_dado(dados_rolados, dados_guardados, indice):
+    valor= dados_rolados[indice]
+    novos_rolados= dados_rolados+[valor]
+    novos_guardados= n_guardados+[valor]
+    dados_escolhidos= [novos_rolados, novos_guardados]
+    return dados_escolhidos
